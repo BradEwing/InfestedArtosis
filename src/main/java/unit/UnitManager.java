f3c8d9413b1dc8dedc0f8a2dc0a9500c4f4b87e8
Brad Ewing <bradleywewing@gmail.com>
2022-11-06 11:33:53 -0800

    feat: track enemy last known locations fix: don't scout base locations of enemy building location known

--- src/main/java/unit/UnitManager.java
+++ src/main/java/unit/UnitManager.java
@@ -160,7 +160,7 @@ public class UnitManager {
         if (unitType == UnitType.Zerg_Drone || unitType == UnitType.Zerg_Larva) {
             ManagedUnit managedWorker = new ManagedUnit(game, unit, UnitRole.IDLE);
             workerManager.onUnitComplete(managedWorker);
-        } else if (unitType == UnitType.Zerg_Overlord || informationManager.getEnemyBuildings().size() + informationManager.getEnemyUnits().size() == 0) {
+        } else if (unitType == UnitType.Zerg_Overlord || informationManager.getEnemyBuildings().size() + informationManager.getVisibleEnemyUnits().size() == 0) {
             createScout(unit);
         } else {
             ManagedUnit managedFighter = new ManagedUnit(game, unit, UnitRole.FIGHT);
@@ -319,7 +319,7 @@ public class UnitManager {
 
     private void assignClosestEnemyToManagedUnit(ManagedUnit managedUnit) {
         List<Unit> enemyUnits = new ArrayList<>();
-        enemyUnits.addAll(informationManager.getEnemyUnits());
+        enemyUnits.addAll(informationManager.getVisibleEnemyUnits());
         enemyUnits.addAll(informationManager.getEnemyBuildings());
 
         if (enemyUnits.size() > 0) {
