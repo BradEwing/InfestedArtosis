c76c42911adfd87fb6f55f970b21241b45a1214f
Brad Ewing <bradleywewing@gmail.com>
2022-11-07 22:21:25 -0800

    - refactor `strategy` to `opener` to prepare for strategy impl

--- src/main/java/strategy/openers/FivePool.java
+++ src/main/java/strategy/openers/FivePool.java
@@ -1,26 +1,25 @@
-package strategy.strategies;
+package strategy.openers;
 
 import bwapi.UnitType;
-import bwapi.UpgradeType;
 import planner.PlannedItem;
-import strategy.Strategy;
+import strategy.Opener;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class FourPool implements Strategy {
+public class FivePool implements Opener {
 
     public List<PlannedItem> getBuildOrder() {
 
         List<PlannedItem> list = new ArrayList<>();
-        list.add(new PlannedItem(UnitType.Zerg_Spawning_Pool, 0, true, true ));
-        list.add(new PlannedItem(UnitType.Zerg_Drone, 1, false, true));
+        list.add(new PlannedItem(UnitType.Zerg_Drone, -1, false, true));
+        list.add(new PlannedItem(UnitType.Zerg_Spawning_Pool, 0, true, true));
+        list.add(new PlannedItem(UnitType.Zerg_Drone, 1, false, false));
         list.add(new PlannedItem(UnitType.Zerg_Zergling, 2, false, false));
         list.add(new PlannedItem(UnitType.Zerg_Zergling, 2, false, false));
         list.add(new PlannedItem(UnitType.Zerg_Zergling, 2, false, false));
         list.add(new PlannedItem(UnitType.Zerg_Zergling, 2, false, false));
-        list.add(new PlannedItem(UnitType.Zerg_Zergling, 2, false, false));
-
+        
         return list;
     }
 
@@ -28,5 +27,5 @@ public class FourPool implements Strategy {
         return false;
     }
 
-    public boolean isAllIn() { return true; }
+    public boolean isAllIn() { return false; }
 }
--- src/main/java/strategy/openers/NinePoolSpeed.java
+++ src/main/java/strategy/openers/NinePoolSpeed.java
@@ -1,14 +1,14 @@
-package strategy.strategies;
+package strategy.openers;
 
 import bwapi.UnitType;
 import bwapi.UpgradeType;
 import planner.PlannedItem;
-import strategy.Strategy;
+import strategy.Opener;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class OverPool implements Strategy {
+public class NinePoolSpeed implements Opener {
 
     public List<PlannedItem> getBuildOrder() {
 
@@ -18,14 +18,15 @@ public class OverPool implements Strategy {
         list.add(new PlannedItem(UnitType.Zerg_Drone, 0, false, false));
         list.add(new PlannedItem(UnitType.Zerg_Drone, 0, false, false));
         list.add(new PlannedItem(UnitType.Zerg_Drone, 0, false, false));
-        list.add(new PlannedItem(UnitType.Zerg_Overlord, 1, false, false));
-        list.add(new PlannedItem(UnitType.Zerg_Spawning_Pool, 2, true, true));
+        list.add(new PlannedItem(UnitType.Zerg_Spawning_Pool, 1, true, true));
+        list.add(new PlannedItem(UnitType.Zerg_Overlord, 2, false, false));
+        list.add(new PlannedItem(UnitType.Zerg_Extractor, 3, true, false));
         list.add(new PlannedItem(UnitType.Zerg_Drone, 3, false, false));
-        list.add(new PlannedItem(UnitType.Zerg_Drone, 3, false, false));
-        list.add(new PlannedItem(UnitType.Zerg_Drone, 3, false, false));
+        list.add(new PlannedItem(UnitType.Zerg_Drone, 4, false, false));
         list.add(new PlannedItem(UnitType.Zerg_Zergling, 4, false, false));
         list.add(new PlannedItem(UnitType.Zerg_Zergling, 4, false, false));
         list.add(new PlannedItem(UnitType.Zerg_Zergling, 4, false, false));
+        list.add(new PlannedItem(UpgradeType.Metabolic_Boost, 5, true));
 
         return list;
     }
