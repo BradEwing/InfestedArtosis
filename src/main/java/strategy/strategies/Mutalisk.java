9a035298dce28d280ff17c9bac8be34b99234f11
Brad Ewing <bradleywewing@gmail.com>
2023-03-15 22:03:31 -0700

    - don't build scourge for now - increase zergling weight for muta strategy

--- src/main/java/strategy/strategies/Mutalisk.java
+++ src/main/java/strategy/strategies/Mutalisk.java
@@ -7,9 +7,9 @@ public class Mutalisk implements Strategy {
 
     public UnitWeights getUnitWeights() {
         UnitWeights weights = new UnitWeights();
-        weights.setWeight(UnitType.Zerg_Zergling, 0.1);
-        weights.setWeight(UnitType.Zerg_Mutalisk, 0.8);
-        weights.setWeight(UnitType.Zerg_Scourge, 0.1);
+        weights.setWeight(UnitType.Zerg_Zergling, 0.3);
+        weights.setWeight(UnitType.Zerg_Mutalisk, 0.7);
+        weights.setWeight(UnitType.Zerg_Scourge, 0.0);
         return weights;
     }
 
