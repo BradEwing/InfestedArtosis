7fbb86a169eb8c0cc8d6e35c25c6e71da78bfe75
Brad Ewing <bradleywewing@gmail.com>
2025-01-20 00:44:37 -0800

    feat: squads typed by unit, add regrouping (#141)

--- src/main/java/unit/squad/Squad.java
+++ src/main/java/unit/squad/Squad.java
@@ -2,12 +2,16 @@ package unit.squad;
 
 import bwapi.Position;
 import bwapi.TilePosition;
+import bwapi.UnitType;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Getter;
 import unit.managed.ManagedUnit;
+import unit.managed.UnitRole;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -27,6 +31,7 @@ public class Squad {
     private Position center;
     private int radius;
     private SquadStatus status;
+    private UnitType squadType = null;
 
     @Override
     public boolean equals(Object other) {
@@ -58,6 +63,7 @@ public class Squad {
 
     public void onFrame() {
         calculateCenter();
+        checkRegroup();
     }
 
     public void addUnit(ManagedUnit managedUnit) {
@@ -99,5 +105,46 @@ public class Squad {
         }
 
         this.center = new Position(x / members.size(), y / members.size());
+    }
+
+    /*
+     * If 25% of the squad is more than 1.5x the average distance from the center, regroup toward the center.
+     */
+    private boolean grouped() {
+        List<Double> distances = new ArrayList<>();
+        double tot = 0;
+
+        for (ManagedUnit u: members) {
+            double d = center.getDistance(u.getUnit().getPosition());
+            distances.add(d);
+            tot += d;
+        }
+
+        double avg = tot / this.size();
+        int outliers = 0;
+        for (double d: distances) {
+            if (d >= avg * 1.5) {
+                outliers += 1;
+            }
+        }
+
+        return outliers / this.size() < 0.25;
+    }
+
+    private void checkRegroup() {
+        boolean grouped = this.grouped();
+        if (status == SquadStatus.FIGHT && !grouped) {
+            status = SquadStatus.REGROUP;
+            for (ManagedUnit u: members) {
+                u.setRole(UnitRole.RALLY);
+                u.setRallyPoint(center.toTilePosition());
+            }
+        }
+        if (status == SquadStatus.REGROUP && grouped) {
+            for (ManagedUnit u: members) {
+                u.setRole(UnitRole.FIGHT);
+                u.setRallyPoint(rallyPoint);
+            }
+        }
     }
 }
