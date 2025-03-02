c3a9c0a23fba1d176b3fd6ae500eeaa2ac7c5f26
Brad Ewing <bradleywewing@gmail.com>
2025-03-02 02:29:43 -0800

    feat: retreat relative to enemies, better hydra micro (#163)

--- src/main/java/unit/managed/Hydralisk.java
+++ src/main/java/unit/managed/Hydralisk.java
@@ -2,9 +2,22 @@ package unit.managed;
 
 import bwapi.Game;
 import bwapi.Unit;
+import bwapi.UpgradeType;
+import bwapi.WeaponType;
 
 public class Hydralisk extends ManagedUnit {
     public Hydralisk(Game game, Unit unit, UnitRole role) {
         super(game, unit, role);
+    }
+
+    @Override
+    protected int weaponRange(Unit enemy) {
+        boolean isEnemyAir = enemy.isFlying();
+        WeaponType weapon = isEnemyAir ? unit.getType().airWeapon() : unit.getType().groundWeapon();
+        int range = weapon.maxRange();
+        if (this.game.self().getUpgradeLevel(UpgradeType.Grooved_Spines) > 0) {
+            range += 32;
+        }
+        return range;
     }
 }
