63fe0e8f74c6fe4d517a53439214a81b8357f861
Brad Ewing <bradleywewing@gmail.com>
2025-06-20 00:01:51 -0400

    feat: consider closest location to drone for building and geyser assignment (#209)

---
+++ src/main/java/unit/managed/ManagedUnitToPositionComparator.java
@@ -0,0 +1,23 @@
+package unit.managed;
+
+import bwapi.Position;
+
+import java.util.Comparator;
+
+/**
+ * ManagedUnitToPositionComparator sorts a list of managed units by distance to the target position.
+ */
+public class ManagedUnitToPositionComparator implements Comparator<ManagedUnit> {
+
+    private Position target;
+    public ManagedUnitToPositionComparator(Position target) {
+        this.target = target;
+    }
+
+    @Override
+    public int compare(ManagedUnit x, ManagedUnit y) {
+        double distanceX = target.getDistance(x.getUnit().getPosition());
+        double distanceY = target.getDistance(y.getUnit().getPosition());
+        return Double.compare(distanceX, distanceY);
+    }
+}
