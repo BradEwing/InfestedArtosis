822fdc7da2a310fbed76952fb7140cc12a11fcca
Brad Ewing <bradleywewing@gmail.com>
2025-11-20 02:30:08 -0800

    fix setup script

--- .github/scripts/setup-sc-docker.sh
+++ .github/scripts/setup-sc-docker.sh
@@ -3,18 +3,19 @@ set -e -x -o pipefail
 
 CACHE_FILE="/tmp/docker-cache/starcraft-game.tar"
 
+git clone https://github.com/Bytekeeper/sc-docker.git
+cd sc-docker
+
+pip3 install setuptools wheel
+python3 setup.py bdist_wheel
+pip3 install dist/basil_scbw-1.1.0-py3-none-any.whl
+
 if [ -f "$CACHE_FILE" ]; then
   echo "Loading cached Docker images..."
   docker load -i "$CACHE_FILE"
 else
   echo "No cache found, building images..."
   
-  git clone https://github.com/Bytekeeper/sc-docker.git
-  ls -la
-  cd sc-docker
-  pip3 install setuptools wheel
-  python3 setup.py bdist_wheel
-  pip3 install dist/basil_scbw-1.1.0-py3-none-any.whl
   cd docker
   ./build_images.sh
   cd ../..
