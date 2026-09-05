cf2ec586dfc73c829e14a8a1869b0230e696988e
Brad Ewing <bradleywewing@gmail.com>
2026-09-05 01:53:45 -0700

    feat(IA-311): retune ZvP engage and retreat thresholds for the new strength scale

--- src/main/java/unit/squad/horizon/HorizonCombatSimulator.java
+++ src/main/java/unit/squad/horizon/HorizonCombatSimulator.java
@@ -527,7 +527,7 @@ public class HorizonCombatSimulator implements CombatSimulator {
     private double engageThreshold(Race opponentRace) {
         switch (opponentRace) {
             case Terran:  return 1.4;
-            case Protoss: return 1.4;
+            case Protoss: return 0.9;
             case Zerg:    return 1.3;
             default:      return DEFAULT_ENGAGE_THRESHOLD;
         }
@@ -536,7 +536,7 @@ public class HorizonCombatSimulator implements CombatSimulator {
     private double retreatThreshold(Race opponentRace) {
         switch (opponentRace) {
             case Terran:  return 1.0;
-            case Protoss: return 0.8;
+            case Protoss: return 0.5;
             case Zerg:    return 0.7;
             default:      return DEFAULT_RETREAT_THRESHOLD;
         }
